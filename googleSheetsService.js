6c57a1f594cdfa75c9095fa7f0d187d85d067b2b
Kyle Summers <kylesu@ping.com>
2024-09-23 08:14:11 -0700

    restructure of google sheet function calls

--- googleSheetsService.js
+++ googleSheetsService.js
@@ -36,39 +36,3 @@ module.exports = {
   getSpreadSheet,
   getSpreadSheetValues
 }
-
-
-// const { GoogleSpreadsheet } = require('google-spreadsheet');
-// const { JWT } = require('google-auth-library');
-// // import { JWT } from 'google-auth-library';
-
-// async function accessSpreadsheet() {
-//     const serviceAccountAuth = new JWT({
-//         email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-//         key: process.env.GOOGLE_PRIVATE_KEY,
-//         scopes: [
-//             'https://www.googleapis.com/auth/spreadsheets',
-//         ],
-//     });
-    
-//     const doc = new GoogleSpreadsheet('1rwQWU2P8-97AHKGgX9ohI9WlIpxjde75zPm3tpmDitI', serviceAccountAuth);
-
-//     // Load the document properties and worksheets
-//     await doc.loadInfo();
-//     console.log(`Spreadsheet title: ${doc.title}`);
-
-//     const sheet = doc.sheetsByIndex[0]; // Assuming you want the first sheet
-//     await sheet.loadHeaderRow(); // Load header row to use headers in parsing
-
-//     const rows = await sheet.getRows(); // Fetch rows
-
-//     // Parse each row for certain items
-//     rows.forEach((row, index) => {
-//         console.log(`Row ${index + 1}:`);
-//         // console.log(`Column1: ${row['Column1 Header Name']}`);
-//         // console.log(`Column2: ${row['Column2 Header Name']}`);
-//         // Add more columns as needed
-//     });
-// }
-
-// accessSpreadsheet().catch(console.error);
--- index.js
+++ index.js
@@ -1,45 +1,34 @@
+const { parse } = require('dotenv');
 const {
     getAuthToken,
-    getSpreadSheet,
     getSpreadSheetValues
   } = require('./googleSheetsService.js');
   
-//   const spreadsheetId = process.argv[2];
-  const spreadsheetId = '1rwQWU2P8-97AHKGgX9ohI9WlIpxjde75zPm3tpmDitI';
-//   const sheetName = process.argv[3];
-  const sheetName = 'Sheet1';
+const spreadsheetId = '1rwQWU2P8-97AHKGgX9ohI9WlIpxjde75zPm3tpmDitI';
+const sheetName = 'Sheet1';
 
-  
-  async function testGetSpreadSheet() {
-    try {
-      const auth = await getAuthToken();
-      const response = await getSpreadSheet({
-        spreadsheetId,
-        auth
-      })
-      console.log('output for getSpreadSheet', JSON.stringify(response.data, null, 2));
-    } catch(error) {
-      console.log(error.message, error.stack);
-    }
+
+
+async function getValues() {
+  try {
+    const auth = await getAuthToken();
+    const response = await getSpreadSheetValues({
+      spreadsheetId,
+      sheetName,
+      auth
+    })
+    return JSON.stringify(response.data, null, 2);
+    // console.log('output for getSpreadSheetValues', JSON.stringify(response.data, null, 2));
+  } catch(error) {
+    console.log(error.message, error.stack);
   }
-  
-  async function testGetSpreadSheetValues() {
-    try {
-      const auth = await getAuthToken();
-      const response = await getSpreadSheetValues({
-        spreadsheetId,
-        sheetName,
-        auth
-      })
-      console.log('output for getSpreadSheetValues', JSON.stringify(response.data, null, 2));
-    } catch(error) {
-      console.log(error.message, error.stack);
-    }
-  }
-  
-  function main() {
-    // testGetSpreadSheet();
-    testGetSpreadSheetValues();
-  }
-  
-  main()
+}
+
+
+
+async function main() {
+  const obj = await getValues(); // Await the getValues() call
+  console.log(obj);
+}
+
+main()
